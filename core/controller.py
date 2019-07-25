3acf0e73c7067b37d8cf5810be9db64cea201ec1
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2019-07-25 03:58:42 +0200

    feat(core.controller.BaseController): refactor -> return bool

--- core/controller.py
+++ core/controller.py
@@ -12,52 +12,47 @@ class BaseController:
     def __init__(self, kind: str, neutral: float, delta_max: float, delta_min: float):
         assert isinstance(kind, str)
         assert kind in ('CUT_IN', 'CUT_OUT')
-        assert isinstance(delta_max, float)
-        assert isinstance(delta_min, float)
+        assert isinstance(float(neutral), float)
+        assert isinstance(float(delta_max), float)
+        assert isinstance(float(delta_min), float)
 
         self.kind: str = kind
-        self.neutral: float = neutral
-        self.delta_max: float = delta_max
-        self.delta_min: float = delta_min
+        self.neutral: float = float(neutral)
+        self.delta_max: float = float(delta_max)
+        self.delta_min: float = float(delta_min)
         self.max_point = neutral + delta_max
         self.min_point = neutral - delta_min
-        self.state: int = 0
+        self.state: bool = False
         self.current_value: float = 0.0
 
-    def set_current_value(self, current_value: float):
-        """
-        set current value
-        :param current_value:
-        :return:
-        """
-        self.current_value = current_value
-
-    def controller(self) -> int:
+    def get_action(self, current_value: float) -> bool:
         """
         call this method for chose action
-        :param current_value:
         :return:
         """
+
+        self.current_value = current_value
+
         if self.kind == 'CUT_IN':
             # neutral point => do nothing
             # or condition can be used here (less readable)
             if self.min_point <= self.current_value <= self.max_point:
-                self.state = 0
+                self.state = False
             # mini point reached, cut in => deactivate
             if self.current_value <= self.min_point:
-                self.state = 1
+                self.state = True
             # max point reached, cut in => activate
             elif self.current_value >= self.max_point:
-                self.state = -1
+                self.state = False
 
-        elif self.kind == 'CUT_OUT':
+        if self.kind == 'CUT_OUT':
             # neutral point => do nothing
             if self.min_point <= self.current_value <= self.max_point:
-                self.state = 0
+                self.state = False
             # mini point reached , cut out => activate
             elif self.current_value <= self.min_point:
-                self.state = -1
+                self.state = False
             elif self.current_value >= self.max_point:
-                self.state = 1
+                self.state = True
 
         return self.state
