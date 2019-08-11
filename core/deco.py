f1e710894996b0c872f933a46f7ef6cbc3645043
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2019-08-11 14:31:50 +0200

    feat(core.deco.HEADER) : header for all sub sys

---
+++ core/deco.py
@@ -0,0 +1,22 @@
+"""
+Printing / header strings
+Author : Shanmugathas Vigneswaran
+"""
+
+
+HEADER: str = """
+       ,.,
+      MMMM_    ,..,
+        \"_ \"__\"MMMMM          ,...,,            Plant-keeper
+ ,..., __.\" --\"    ,.,     _-\"MMMMMMM           Version : {version}
+MMMMMM\"___ \"_._   MMM\"_.\"\" _ \"\"\"\"\"\"             Git: {repo}
+ \"\"\"\"\"    \"\" , \_.   \"_. .\"                     Loaded subsystem: {sub_sys}
+        ,., _\"__ \__./ .\"                       Sub system Info: 
+       MMMMM_\"  \"_    ./
+        ''''      (    )
+ ._______________.-'____\"---._.
+  \                          /
+   \________________________/
+   (_)                    (_)
+   Corwyn Yasuo Miyagishima
+"""
