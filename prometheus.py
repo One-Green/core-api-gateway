3d84419d4c29edb0a064e193db9635e8d5739ee6
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2019-08-17 15:02:57 +0200

    feat(prometheus.py): sensors server

--- prometheus.py
+++ prometheus.py
@@ -3,7 +3,7 @@ import sys
 import time
 import random
 import django
-from prometheus_client import Gauge, start_http_server
+from prometheus_client import Gauge, start_http_server, Enum
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plant_kiper.settings")
 sys.path.append(os.path.dirname('__file__'))
@@ -12,24 +12,41 @@ django.setup()
 from plant_kiper.settings import __version__, __current_repo__
 from plant_core.models import Enclosure
 from core.deco import HEADER
+from core.uv_helpers import UV_INDEX
 
-SUB_SYS: str = 'PROMETHEUS REPORT'
-INFO: str = 'Server created: localhost:8000 to expose open metrics data'
+SUB_SYS: str = 'Prometheus client'
+INFO: str = 'Server created at http://localhost:8000, ensure Prometheus Server is running'
 
 # Create sensors
 enclosure_temperature = Gauge('enclosure_temperature', 'Enclosure temperature')
+enclosure_hygrometry = Gauge('enclosure_hygrometry', 'Enclosure hygrometry')
+enclosure_co2_ppm = Gauge('enclosure_co2_ppm', 'Enclosure CO2 ppm')
+enclosure_voc_ppm = Gauge('enclosure_voc_ppm', 'Enclosure VOC ppm')
+# enclosure_uv_index = Gauge('enclosure_uv_index', 'Enclosure UV index')
+enclosure_uv_index = Enum('enclosure_uv_index', 'Enclosure UV index',
+                          states=UV_INDEX)
 
 
-def main():
+# Devices status
+
+def main_dummies():
+    """
+    populate fake metrics until first release
+    :return:
+    """
     while True:
-        rand = float(random.randint(0, 100))
-        enclosure_temperature.set(rand)
+        enclosure_temperature.set(float(random.randint(12, 30)))
+        enclosure_hygrometry.set(float(random.randint(20, 90)))
+        enclosure_co2_ppm.set(float(random.randint(300, 1500)))
+        enclosure_voc_ppm.set(float(random.randint(500, 4000)))
+        enclosure_uv_index.state(UV_INDEX[random.randint(0, 8)])
+        time.sleep(1)
 
 
 if __name__ == '__main__':
     print(HEADER.format(version=__version__,
                         repo=__current_repo__,
-                        sub_sys=SUB_SYS))
+                        sub_sys=SUB_SYS,
+                        sub_sys_info=INFO))
     start_http_server(8000)
-    main()
-    time.sleep(1)
+    main_dummies()
