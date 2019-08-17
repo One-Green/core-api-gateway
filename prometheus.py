14a0ad4df37bb8c780a810ff83efbe4d3f83c393
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2019-08-18 00:42:09 +0200

    feat(prometheus.py) : dummies data to real data from db

--- prometheus.py
+++ prometheus.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.dirname('__file__'))
 django.setup()
 
 from plant_kiper.settings import __version__, __current_repo__
-from plant_core.models import Enclosure
+from plant_core.models import Enclosure, Cooler, Heater
 from core.deco import HEADER
 from core.uv_helpers import UV_INDEX
 
@@ -26,8 +26,23 @@ enclosure_voc_ppm = Gauge('enclosure_voc_ppm', 'Enclosure VOC ppm')
 enclosure_uv_index = Enum('enclosure_uv_index', 'Enclosure UV index',
                           states=UV_INDEX)
 
-
 # Devices status
+cooler_status = Gauge('device_cooler_status', 'Cooler power status')
+heater_cooler_status = Gauge('device_heater_status', 'Heater power status')
+
+
+def main():
+    """
+
+    :return:
+    """
+    while True:
+        enclosure_temperature.set(Enclosure.get_status()['enclosure_temperature'])
+        # devices power status
+        cooler_status.set(float(Cooler.get_status()['power_status']))
+        heater_cooler_status.set(float(Heater.get_status()['power_status']))
+        time.sleep(1)
+
 
 def main_dummies():
     """
@@ -35,11 +50,17 @@ def main_dummies():
     :return:
     """
     while True:
+        # enclosure sensors
         enclosure_temperature.set(float(random.randint(12, 30)))
         enclosure_hygrometry.set(float(random.randint(20, 90)))
         enclosure_co2_ppm.set(float(random.randint(300, 1500)))
         enclosure_voc_ppm.set(float(random.randint(500, 4000)))
         enclosure_uv_index.state(UV_INDEX[random.randint(0, 8)])
+
+        # devices power status
+        cooler_status.set(float(Cooler.get_status()['power_status']))
+        heater_cooler_status.set(float(Heater.get_status()['power_status']))
+
         time.sleep(1)
 
 
@@ -49,4 +70,4 @@ if __name__ == '__main__':
                         sub_sys=SUB_SYS,
                         sub_sys_info=INFO))
     start_http_server(8000)
-    main_dummies()
+    main()
