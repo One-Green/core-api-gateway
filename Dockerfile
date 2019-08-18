041f829fa495f8426058f9bcda2629fa19c14300
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2019-08-18 20:49:18 +0200

    ci(Dockerfile, init_plant_config.py): init dummies params for plant

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,7 @@ RUN pipenv run python manage.py makemigrations plant_core --noinput
 RUN pipenv run python manage.py migrate
 RUN pipenv run python manage.py collectstatic --noinput
 RUN pipenv run python create_plant_keeper_user.py
+RUN pipenv run python init_plant_config.py
 
 CMD pipenv run gunicorn --workers=3 --bind 0.0.0.0:8001 --daemon plant_kiper.wsgi  &&\
     pipenv run python prometheus.py && \
---
+++ init_plant_config.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import django
+from datetime import time
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plant_kiper.settings")
+sys.path.append(os.path.dirname(os.path.dirname(os.path.join(os.path.dirname('__file__')))))
+django.setup()
+from plant_core.models import PlantSettings
+
+PlantSettings(plant_identifier='auto_init',
+              plant_type='auto_init',
+              air_temperature=20.,
+              air_hygrometry=50,
+              air_co2_ppm=5000.,
+              soil_hygrometry=50.,
+              light_start=time(7,0),
+              light_end=time(20,0)).save()
