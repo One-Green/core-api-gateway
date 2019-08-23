69813dcade0b3d95099ab055a768be278c710f26
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2019-08-23 12:43:43 +0200

    feat(plant_kiper.settings.DATABASES) : move sqlite to Postgres

--- plant_kiper/settings.py
+++ plant_kiper/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'plant_kiper.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'HOST': 'db',
+            'PORT': 5432,
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
