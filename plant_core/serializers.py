3f996b25fac3bdfac87dbb617afe51b54c52f86e
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2020-04-04 14:28:58 +0200

    feat(plant_core.admin, plant_core.serializers, plant_core.views): upgrade orm

--- plant_core/serializers.py
+++ plant_core/serializers.py
@@ -1,73 +1,17 @@
 from rest_framework import serializers
 from plant_core.models import (
-    Enclosure,
-    Cooler,
-    AirHumidifier,
-    WaterPump,
-    Heater,
-    UvLight,
-    CO2Valve,
-    Filters,
-    SimpleFlaps
+    EnclosureSensor
 )
 
 
 class EnclosureSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Enclosure
+        model = EnclosureSensor
         fields = '__all__'
 
 
-class CoolerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cooler
-        fields = '__all__'
-
-
-class AirHumidifierSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AirHumidifier
-        fields = '__all__'
-
-
-class WaterPumpSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WaterPump
-        fields = '__all__'
-
-
-class SprinklerValveSerializer(serializers.Serializer):
+class SprinklerSerializer(serializers.Serializer):
     tag = serializers.CharField()
-    # TODO fix read/write mandatory field
-    # soil_hygrometry = serializers.FloatField(write_only=True)
-    # power_status = serializers.IntegerField(read_only=True)
+    soil_humidity = serializers.FloatField(write_only=True)
+    power = serializers.BooleanField(read_only=True, default=False)
 
-
-class HeaterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Heater
-        fields = '__all__'
-
-
-class UvLightSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UvLight
-        fields = '__all__'
-
-
-class CO2ValveSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CO2Valve
-        fields = '__all__'
-
-
-class FiltersSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Filters
-        fields = '__all__'
-
-
-class SimpleFlapsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SimpleFlaps
-        fields = '__all__'
--- plant_core/views.py
+++ plant_core/views.py
@@ -5,30 +5,14 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from plant_core.models import (
-    Enclosure,
-    Cooler,
-    AirHumidifier,
-    WaterPump,
+    SprinklerSoilHumiditySensor,
     SprinklerTag,
     SprinklerValve,
-    Heater,
-    UvLight,
-    CO2Valve,
-    Filters,
-    SimpleFlaps
 )
 
 from plant_core.serializers import (
     EnclosureSerializer,
-    CoolerSerializer,
-    AirHumidifierSerializer,
-    WaterPumpSerializer,
-    SprinklerValveSerializer,
-    HeaterSerializer,
-    UvLightSerializer,
-    CO2ValveSerializer,
-    FiltersSerializer,
-    SimpleFlapsSerializer
+    SprinklerSerializer
 )
 
 
@@ -48,84 +32,13 @@ class EnclosureView(GenericAPIView):
             status=status.HTTP_201_CREATED
         )
 
-    @csrf_exempt
-    def get(self, request):
-        serializer = EnclosureSerializer(Enclosure.objects.all(), many=True)
-        return Response(serializer.data[-1], status=status.HTTP_200_OK)
 
-
-class CoolerView(GenericAPIView):
-    serializer_class = CoolerSerializer
+class SprinklerView(GenericAPIView):
+    serializer_class = SprinklerSerializer
 
     @csrf_exempt
     def post(self, request):
-        serializer = CoolerSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(
-            {
-                'type': "CoolerView",
-                'saved': True
-            },
-            status=status.HTTP_201_CREATED
-        )
-
-    @csrf_exempt
-    def get(self, request):
-        serializer = CoolerSerializer(Cooler.objects.all(), many=True)
-        return Response(serializer.data[-1], status=status.HTTP_200_OK)
-
-
-class AirHumidifierView(GenericAPIView):
-    serializer_class = AirHumidifierSerializer
-
-    @csrf_exempt
-    def post(self, request):
-        serializer = AirHumidifierSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(
-            {
-                'type': "AirHumidifierView",
-                'saved': True
-            },
-            status=status.HTTP_201_CREATED
-        )
-
-    @csrf_exempt
-    def get(self, request):
-        serializer = AirHumidifierSerializer(AirHumidifier.objects.all(), many=True)
-        return Response(serializer.data[-1], status=status.HTTP_200_OK)
-
-
-class WaterPumpView(GenericAPIView):
-    serializer_class = WaterPumpSerializer
-
-    @csrf_exempt
-    def post(self, request):
-        serializer = WaterPumpSerializer(data=request.data)
-        if serializer.is_valid():
-            WaterPump.set_sensors(request.data)
-        return Response(
-            {
-                'type': "WaterPumpView",
-                'saved': True
-            },
-            status=status.HTTP_201_CREATED
-        )
-
-    @csrf_exempt
-    def get(self, request):
-        serializer = WaterPumpSerializer(WaterPump.objects.all(), many=True)
-        return Response(serializer.data[-1], status=status.HTTP_200_OK)
-
-
-class SprinklerValveView(GenericAPIView):
-    serializer_class = SprinklerValveSerializer
-
-    @csrf_exempt
-    def post(self, request):
-        serializer = SprinklerValveSerializer(data=request.data)
+        serializer = SprinklerSerializer(data=request.data)
         if serializer.is_valid():
             tag_created = False
             try:
@@ -135,17 +48,22 @@ class SprinklerValveView(GenericAPIView):
                 tag.save()
                 tag_created = True
             finally:
-                SprinklerValve.set_sensors(
-                    tag,
-                    request.data
-                )
+                SprinklerSoilHumiditySensor(
+                    tag=tag,
+                    soil_humidity=request.data['soil_humidity']
+                               ).save()
+            try:
+                power = SprinklerValve.objects.filter(tag=tag)[0].power
+            except IndexError:
+                power = False
 
             return Response(
                 {
                     'type': "SprinklerValveView",
                     'tag': tag.tag,
                     'tag_created': tag_created,
-                    'saved': True
+                    'saved': True,
+                    'power': power
                 },
                 status=status.HTTP_201_CREATED
             )
@@ -154,151 +72,8 @@ class SprinklerValveView(GenericAPIView):
                 {
                     'type': "SprinklerValveView",
                     'error': True,
-                    'message': 'Missing "tag", "soil_hygrometry" to process'
+                    'message': 'Missing "tag", "soil_humidity" to process',
+                    'power': False
                 },
                 status=status.HTTP_400_BAD_REQUEST
             )
-
-    @csrf_exempt
-    def get(self, request):
-        serializer = SprinklerValveSerializer(data=request.data)
-        if serializer.is_valid():
-            try:
-                tag = SprinklerTag.objects.get(tag=request.data['tag'])
-            except SprinklerTag.DoesNotExist:
-                return Response(
-                    {
-                        'type': "SprinklerValveView",
-                        'tag': request.data['tag'],
-                        'error': True,
-                        'saved': False,
-                        'message': "Tag not found in database"
-                    },
-                    status=status.HTTP_417_EXPECTATION_FAILED
-                )
-
-            r = model_to_dict(
-                SprinklerValve.objects.filter(
-                    tag=tag
-                ).order_by('-created')[0]
-            )
-            return Response(r, status=status.HTTP_200_OK)
-        else:
-            return Response(
-                {
-                    'type': "SprinklerValveView",
-                    'error': True,
-                    'message': 'Serializer not validated this request'
-                },
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-
-class HeaterView(GenericAPIView):
-    serializer_class = HeaterSerializer
-
-    @csrf_exempt
-    def post(self, request):
-        serializer = HeaterSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(
-            {
-                'type': "HeaterView",
-                'saved': True
-            },
-            status=status.HTTP_201_CREATED
-        )
-
-    @csrf_exempt
-    def get(self, request):
-        serializer = HeaterSerializer(Heater.objects.all(), many=True)
-        return Response(serializer.data[-1], status=status.HTTP_200_OK)
-
-
-class UvLightView(GenericAPIView):
-    serializer_class = UvLightSerializer
-
-    @csrf_exempt
-    def post(self, request):
-        serializer = UvLightSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(
-            {
-                'type': "UvLightView",
-                'saved': True
-            },
-            status=status.HTTP_201_CREATED
-        )
-
-    @csrf_exempt
-    def get(self, request):
-        serializer = UvLightSerializer(UvLight.objects.all(), many=True)
-        return Response(serializer.data[-1], status=status.HTTP_200_OK)
-
-
-class CO2ValveView(GenericAPIView):
-    serializer_class = CO2ValveSerializer
-
-    @csrf_exempt
-    def post(self, request):
-        serializer = CO2ValveSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(
-            {
-                'type': "CO2ValveView",
-                'saved': True
-            },
-            status=status.HTTP_201_CREATED
-        )
-
-    @csrf_exempt
-    def get(self, request):
-        serializer = CO2ValveSerializer(CO2Valve.objects.all(), many=True)
-        return Response(serializer.data[-1], status=status.HTTP_200_OK)
-
-
-class FiltersView(GenericAPIView):
-    serializer_class = FiltersSerializer
-
-    @csrf_exempt
-    def post(self, request):
-        serializer = FiltersSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(
-            {
-                'type': "FiltersView",
-                'saved': True
-            },
-            status=status.HTTP_201_CREATED
-        )
-
-    @csrf_exempt
-    def get(self, request):
-        serializer = FiltersView(Filters.objects.all(), many=True)
-        return Response(serializer.data[-1], status=status.HTTP_200_OK)
-
-
-class SimpleFlapsView(GenericAPIView):
-    serializer_class = SimpleFlapsSerializer
-
-    @csrf_exempt
-    def post(self, request):
-        serializer = SimpleFlapsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(
-            {
-                'type': "SimpleFlapsView",
-                'saved': True
-            },
-            status=status.HTTP_201_CREATED
-        )
-
-    @csrf_exempt
-    def get(self, request):
-        serializer = SimpleFlapsSerializer(SimpleFlaps.objects.all(), many=True)
-        return Response(serializer.data[-1], status=status.HTTP_200_OK)
