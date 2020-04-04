c27df7f3e476073ce7fec356f296b3e38eb87303
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2020-04-04 18:09:25 +0200

    feat(controllers.loki_tag): loki logging tag "message" definition

---
+++ controllers/loki_tag.py
@@ -0,0 +1,9 @@
+"""
+Loki logging
+message to tag for controllers
+
+
+"""
+SENSOR_NOT_UPDATED = 'sensor_not_updated'
+SETTING_NOT_FOUND = 'controller_setting_not_found'
+TAG_NOT_FOUND = 'tag_not_found'
