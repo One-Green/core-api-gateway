907abbdba6295c908ac0ad70376caa6d0404ff7a
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2020-04-04 18:43:58 +0200

    feat(core.controller.BinaryController): new binary controller

--- core/controller.py
+++ core/controller.py
@@ -12,98 +12,70 @@ Licence:  https://creativecommons.org/licenses/by-nc/4.0/
 from datetime import time, datetime
 
 
-class BaseController:
+class BinaryController:
     """
     binary controller class
     """
 
     def __init__(
             self,
-            kind: str,
-            neutral: float,
-            delta_max: float = 0,
-            delta_min: float = 0,
+            _max: float = 0,
+            _min: float = 0,
             reverse: bool = False
     ):
-        assert isinstance(kind, str)
-        assert kind in ('CUT_IN', 'CUT_OUT')
-        assert isinstance(float(neutral), float)
-        assert isinstance(float(delta_max), float)
-        assert isinstance(float(delta_min), float)
+
+        assert isinstance(float(_max), float)
+        assert isinstance(float(_min), float)
         assert isinstance(reverse, bool)
 
-        self.kind: str = kind
-        self.neutral: float = float(neutral)
-        self.delta_max: float = float(delta_max)
-        self.delta_min: float = float(delta_min)
-        self.max_point = neutral + delta_max
-        self.min_point = neutral - delta_min
-        self.state: int = 0
-        self.current_value: float = 0.0
+        self._max = _max
+        self._min = _min
         self.reverse = reverse
+        self.__validate()
+        self.__swap_min_max()
 
-    def set_sensor_value(
-            self,
-            current_value: float
-    ) -> None:
-        """
-        set read sensor value
-        :param current_value:
-        :return:
-        """
-        self.current_value = current_value
+    def __validate(self):
+        if self._min > self._max:
+            raise ValueError(f'{self._min} > {self._max}')
+        if self._min == self._max:
+            raise ValueError(f'{self._min} == {self._max}')
 
-    def __cut_in_handler(self) -> None:
-        """
-        cut in kind handler
-        :return:
-        """
-        # neutral point => do nothing
-        # or condition can be used here (less readable)
-        if self.min_point <= self.current_value <= self.max_point:
-            self.state = 0
-        # mini point reached, cut in => deactivate
-        if self.current_value <= self.min_point:
-            self.state = 1
-        # max point reached, cut in => activate
-        elif self.current_value >= self.max_point:
-            self.state = 0
+    def __swap_min_max(self):
+        if self.reverse:
+            _max = self._min
+            _min = self._max
+            self._max = _max
+            self._min = _min
 
-    def __cut_out_handler(self) -> None:
-        """
-        cut out kind handler
-        :return:
-        """
-        if self.current_value <= self.min_point:
-            if self.reverse:
-                self.state = 1
-            else:
-                self.state = 0
-        if self.current_value >= self.max_point:
-            if self.reverse:
-                self.state = 0
-            else:
-                self.state = 1
+    def __apply_reverse(self, signal: int):
+        if self.reverse and not signal:
+            return 1
+        elif self.reverse and signal:
+            return 0
+        else:
+            return signal
 
-    @property
-    def action(self) -> int:
-        """
-        get action to do
-        :return:
-        """
-        if self.kind == 'CUT_IN':
-            self.__cut_in_handler()
-            return self.state
+    def get_signal(self, sensor: float):
 
-        if self.kind == 'CUT_OUT':
-            self.__cut_out_handler()
-            return self.state
+        assert isinstance(float(sensor), float)
+        signal: int = 0
+
+        if sensor < self._min:
+            signal = 1
+        elif sensor > self._max:
+            signal = 0
+
+        return self.__apply_reverse(signal)
+
+
+BinaryController(21, 20).get_signal(20)
 
 
 class BaseTimeRangeController:
     """
     Time based binary controller
     """
+
     def __init__(
             self,
             start_at: time,
