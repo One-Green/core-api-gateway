4478e7d72574eaf84d3e243ae699a317bfc2fa62
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2020-04-19 23:23:25 +0200

    feat(controllers.heater): use enclosure temperature + sleep to reduce cpu/mem

--- controllers/heater.py
+++ controllers/heater.py
@@ -7,10 +7,14 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plant_kiper.settings")
 sys.path.append(os.path.dirname(os.path.dirname(os.path.join('..', '..', os.path.dirname('__file__')))))
 django.setup()
 
-from plant_kiper.settings import controller_logger
+from plant_kiper.settings import (
+    controller_logger,
+    CONTROLLERS_LOOP_EVERY
+)
 from controllers import loki_tag
 from core.controller import BinaryController
 from plant_core.models import (
+    EnclosureSensor,
     PlantSettings,
     HeaterSensor,
     Heater
@@ -26,16 +30,18 @@ ctl = BinaryController()
 
 def main():
     setting = PlantSettings.get_settings()
+    enclosure = EnclosureSensor.get_status()
     sensor = HeaterSensor.get_status()
-    if sensor:
+    if enclosure and sensor:
         ctl.set_conf(
             _min=setting.air_temperature_min,
             _max=setting.air_temperature_max,
             reverse=False
         )
-        signal = ctl.get_signal(sensor.air_in_temperature)
+        signal = ctl.get_signal(enclosure.temperature)
 
         Heater(
+            enclosure_temperature=enclosure.temperature,
             temperature_in=sensor.air_in_temperature,
             temperature_level_min=setting.air_temperature_min,
             temperature_level_max=setting.air_temperature_max,
@@ -46,7 +52,7 @@ def main():
                 f'[INFO] [{CONTROLLED_DEVICE}] '
                 f't_min={round(setting.air_temperature_min)}, '
                 f't_max={round(setting.air_temperature_max)} '
-                f't_in={round(sensor.air_in_temperature)} '
+                f't_enclosure={round(enclosure.temperature)} '
                 f"action={signal}"
             ),
             extra={
@@ -71,13 +77,13 @@ def main():
                 }
             }
         )
-        time.sleep(60)
 
 
 if __name__ == '__main__':
     while True:
         if PlantSettings.get_settings().activate_heater_controller:
             main()
+            time.sleep(CONTROLLERS_LOOP_EVERY)
         else:
             print('[INFO] HEATER DEACTIVATED')
             time.sleep(5)
