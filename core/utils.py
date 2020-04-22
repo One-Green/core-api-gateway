58f211f612712a265f04a8d61cb02d9a05cd4489
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2020-04-22 20:56:03 +0200

    feat(core.utils.is_api_gateway_up): readiness probe for Api Gateway

---
+++ core/utils.py
@@ -0,0 +1,19 @@
+import requests
+from requests.exceptions import ConnectionError
+
+
+def is_api_gateway_up(host="http://api-gateway:8001") -> bool:
+    """
+    true if api gateway is ready,
+    mean django make migrations + migrate is done
+    controller can access to ORM
+    :param host:
+    :return:
+    """
+    try:
+        if requests.get(host).ok:
+            return True
+        else:
+            return False
+    except ConnectionError:
+        return False
