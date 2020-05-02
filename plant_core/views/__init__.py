96788ebbb32c496a53260cc08d9bd47be49d336f
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2020-05-02 20:33:59 +0200

    feat(plant_core.views): split view classes

---
+++ plant_core/views/__init__.py
@@ -0,0 +1,6 @@
+
+from .enclosure import EnclosureView
+from .sprinkler import SprinklerView
+from .cooler import CoolerView
+from .heater import HeaterView
+from .air_humidifier import AirHumidifierView
