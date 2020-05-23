4b0bda5fa378831cceaf8742f5a5a18f859f7861
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2020-05-23 21:07:34 +0200

    feat(plant_kiper.settings.TEMPLATES): look for templates in apps

--- plant_kiper/settings.py
+++ plant_kiper/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = "plant_kiper.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
