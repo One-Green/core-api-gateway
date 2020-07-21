52edf023ac43cf5d9001265f5053f329f34b925e
Shanmugathas Vigneswaran <vigneswaran.shanmugathas@nokia.com>
2020-07-21 23:56:23 +0200

    feat(tests/): add air_humidiifier.py, enclosure.py

---
+++ tests/air_humidiifier.py
@@ -0,0 +1,34 @@
+import requests
+from pprint import pprint
+import random
+import _pickle
+import time
+
+_list = _pickle.load(open("sign_list.pkl", "rb"))
+
+
+class AirHumidifier:
+    api = ""
+
+    def post(self):
+        _dict = {
+            "air_in_humidity": 0,
+            "air_out_humidity": 0
+        }
+
+        return requests.post(self.api, json=_dict).json()
+
+    def post_sig(self):
+        for _ in _list:
+            _dict = {
+                "air_in_humidity": _,
+                "air_out_humidity": _,
+            }
+
+            pprint(requests.post(self.api, json=_dict).json())
+
+
+if __name__ == "__main__":
+    while True:
+        AirHumidifier().post_sig()
+        time.sleep(0.5)
---
+++ tests/enclosure.py
@@ -0,0 +1,42 @@
+import requests
+from pprint import pprint
+import random
+import _pickle
+import time
+
+_list = _pickle.load(open("sign_list.pkl", "rb"))
+
+
+class Enclosure:
+    api = ""
+
+    def post(self):
+        _dict = {
+            {
+                "temperature": random.randint(20, 50),
+                "humidity": random.randint(20, 50),
+                "uv_index": "string",
+                "co2_ppm": random.randint(20, 50),
+                "cov_ppm": random.randint(20, 50)
+            }
+        }
+
+        return requests.post(self.api, json=_dict).json()
+
+    def post_sig(self):
+        for _ in _list:
+            _dict = {
+                    "temperature": random.randint(20, 50),
+                    "humidity": random.randint(20, 50),
+                    "uv_index": "string",
+                    "co2_ppm": random.randint(20, 50),
+                    "cov_ppm": random.randint(20, 50)
+                }
+
+            pprint(requests.post(self.api, json=_dict).json())
+
+
+if __name__ == "__main__":
+    while True:
+        Enclosure().post_sig()
+        time.sleep(0.5)
