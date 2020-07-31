09672ad5aebc265d0dac774c97e5a3eb6faa6b78
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2020-08-01 00:19:26 +0200

    tests(test_mqtt_consumer.py, test_mqtt_producer.py): init test

---
+++ tests/test_mqtt_producer.py
@@ -0,0 +1,16 @@
+import json
+import random
+import paho.mqtt.client as mqtt
+
+client = mqtt.Client()
+client.connect("localhost", 1883, 60)
+
+while True:
+    _dict = {
+        "value": True,
+        "_dict": {"test": random.randint(0, 100)}
+    }
+    client.publish(
+        'tests/tag-1',
+        json.dumps(_dict)
+    )
