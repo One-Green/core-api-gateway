2e69c158c4858a40dd92b0b2eb659f43296e53e9
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2020-08-05 20:19:00 +0200

    feat(core.utils.py): rest api gateway replaced by MQTT

--- core/utils.py
+++ core/utils.py
@@ -1,19 +1,5 @@
-import requests
-from requests.exceptions import ConnectionError
+from datetime import datetime
 
 
-def is_api_gateway_up(host="http://api-gateway:8001/admin") -> bool:
-    """
-    true if api gateway is ready,
-    mean django make migrations + migrate is done
-    controller can access to ORM
-    :param host:
-    :return:
-    """
-    try:
-        if requests.get(host).ok:
-            return True
-        else:
-            return False
-    except ConnectionError:
-        return False
+def get_now():
+    return datetime.isoformat(datetime.utcnow())
