5a5203763439b6d92852e58183256f73072e2184
Shanmugathas Vigneswaran <vigneswaran.shanmugathas@nokia.com>
2020-08-14 22:07:56 +0200

    chore(project): clean

--- core/registry.py
+++
@@ -1,2 +0,0 @@
-REDIS_SPRINKLER_REGISTRY_KEY: str = 'sprinkler_registry'
-REDIS_DEEP_WATER_CULTURE_REGISTRY_KEY: str = 'dwc_registry'
--- plant_core/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlantCoreConfig(AppConfig):
-    name = "plant_core"
