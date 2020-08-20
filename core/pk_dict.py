32d92ce3d90af5efa9cd0ae7492b8a655f97eef3
Shanmugathas Vigneswaran <vigneswaran.shanmugathas@nokia.com>
2020-08-20 23:59:14 +0200

    feat(core.pk_dict.WaterCtrlDict): rename actuator signal

--- core/pk_dict.py
+++ core/pk_dict.py
@@ -13,5 +13,5 @@ class SprinklerSensorDict(TypedDict):
 
 class WaterCtrlDict(TypedDict):
     water_pump_signal: bool
-    ph_down_pump_signal: bool
-    nutrient_up_pump_signal: bool
+    nutrient_pump_signal: bool
+    ph_downer_pump_signal: bool
