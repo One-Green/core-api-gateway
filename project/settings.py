c4b68940b4e2e3d794ec23d4f93e5ddaa0389f3c
shanisma <shanmugathas.vigneswaran@outlook.fr>
2020-08-23 23:18:44 +0200

    feat(project.settings): add repo and version

--- project/settings.py
+++ project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+__repo__ = "https://github.com/Plant-Keeper/plant-keeper-master"
+__version__ = "beta"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
