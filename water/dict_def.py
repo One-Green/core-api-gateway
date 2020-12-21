834d18ac90f8d93f4aed6ff2594ecce706bbb522
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2020-12-21 23:50:06 +0100

    feat(water.dict_ref): update

--- water/dict_def.py
+++ water/dict_def.py
@@ -2,6 +2,11 @@ from typing import TypedDict
 
 
 class WaterCtrlDict(TypedDict):
-    water_pump_signal: bool
-    nutrient_pump_signal: bool
-    ph_downer_pump_signal: bool
+    controller_type: str
+    water_pump_signal: int
+    nutrient_pump_signal: int
+    ph_downer_pump_signal: int
+    tds_max_level: float
+    tds_min_level: float
+    ph_max_level: float
+    ph_min_level: float
