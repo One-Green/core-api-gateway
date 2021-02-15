3bde20267dd8c11734fdd5f36dfca9d931cd6b88
Vigneswaran Shanmugathas <shanmugathas.vigneswaran-prestataire@laposte.fr>
2021-02-15 23:25:25 +0100

    clean(core.controller.TimeRangeController): init without configuration, use get_signal method like binary controller instead action as property

--- core/controller.py
+++ core/controller.py
@@ -102,24 +102,22 @@ class TimeRangeController:
     Time based binary controller
     """
 
-    def __init__(self, start_at: time, end_at: time, reverse: bool = False):
-
-        assert isinstance(start_at, time)
-        assert isinstance(end_at, time)
-        assert isinstance(reverse, bool)
+    def __init__(self, reverse: bool = False):
 
         self.time_now: time = datetime.now().time()
-        self.start_at: time = start_at
-        self.end_at: time = end_at
+        self.start_at: time
+        self.end_at: time
         self.reverse: bool = reverse
         self.state: int = 0
 
+    def set_conf(self, start_at, end_at):
+        self.start_at: time = start_at
+        self.end_at: time = end_at
+
     def set_current_time(self, time_now: time):
-        assert isinstance(time_now, time)
         self.time_now = time_now
 
-    @property
-    def action(self) -> int:
+    def get_signal(self) -> int:
         if self.start_at <= self.time_now <= self.end_at:
             if self.reverse:
                 self.state = 0
