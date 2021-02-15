2ddc837672510d9d7a593227b4541ebe0b509ebe
Vigneswaran Shanmugathas <shanmugathas.vigneswaran-prestataire@laposte.fr>
2021-02-15 22:29:13 +0100

    fix(sprinkler.serializers.RegistrySerializer.Meta.ref_name): drf use specific ref name

--- sprinkler/serializers.py
+++ sprinkler/serializers.py
@@ -4,6 +4,9 @@ from rest_framework import serializers
 class RegistrySerializer(serializers.Serializer):
     tag = serializers.CharField(write_only=True)
 
+    class Meta:
+        ref_name = "sprinklers"
+
 
 class ConfigSerializer(serializers.Serializer):
     soil_moisture_min_level = serializers.FloatField(write_only=True)
