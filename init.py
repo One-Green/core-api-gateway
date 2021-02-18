48b3c654b242b5b6a728e88e7b539491282cc081
Vigneswaran Shanmugathas <shanmugathas.vigneswaran-prestataire@laposte.fr>
2021-02-18 22:46:43 +0100

    feat(init.py): makemigrations glbl, light

--- init.py
+++ init.py
@@ -7,6 +7,8 @@ sys.path.insert(0, os.path.abspath("."))
 django.setup()
 
 call_command('makemigrations', interactive=False, verbosity=2)
+call_command('makemigrations', 'glbl', interactive=False, verbosity=2)
 call_command('makemigrations', 'sprinkler', interactive=False, verbosity=2)
 call_command('makemigrations', 'water', interactive=False, verbosity=2)
+call_command('makemigrations', 'light', interactive=False, verbosity=2)
 call_command('migrate', interactive=False, verbosity=2)
