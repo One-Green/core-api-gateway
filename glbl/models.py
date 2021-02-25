26abc01d3d39fdc45c908f835ad5546c435c53f6
Vigneswaran Shanmugathas <shanmugathas.vigneswaran-prestataire@laposte.fr>
2021-02-25 18:40:07 +0100

    style(glbl.models): black

--- glbl/models.py
+++ glbl/models.py
@@ -5,27 +5,23 @@ import pytz
 
 def timezone_validator(value):
     if value not in pytz.all_timezones:
-        raise ValidationError(f'[Error] Timezone={value} not recognized in pytz.all_timezones')
+        raise ValidationError(
+            f"[Error] Timezone={value} not recognized in pytz.all_timezones"
+        )
 
 
 class Config(models.Model):
     timezone = models.TextField(
-        unique=True,
-        null=False,
-        blank=False,
-        validators=[timezone_validator]
+        unique=True, null=False, blank=False, validators=[timezone_validator]
     )
 
 
 class GlobalConfig:
-
     def __init__(self):
         pass  # Global model wrapper to use update_or_create
 
     @staticmethod
-    def update_config(
-            data
-    ):
+    def update_config(data):
         timezone_validator(data["timezone"])
         Config.objects.update_or_create(
             defaults={
