5a504c2449fadd179ea182215e24faed4c373c12
Vigneswaran Shanmugathas <shanmugathas.vigneswaran-prestataire@laposte.fr>
2021-02-25 18:08:07 +0100

    clean(core.utils)

--- core/utils.py
+++ core/utils.py
@@ -3,12 +3,3 @@ from datetime import datetime
 
 def get_now():
     return datetime.isoformat(datetime.utcnow())
-
-
-def get_timezone_now(_timezone: str):
-    """
-
-    :param _timezone:
-    :return:
-    """
-    return "pass"
