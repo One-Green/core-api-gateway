3b2532f459011d0cac80d59fdac9eb5728aa1520
Vigneswaran Shanmugathas <shanmugathas.vigneswaran-prestataire@laposte.fr>
2021-03-17 23:45:47 +0100

    fix(sprinkler.dict_def.SprinklerCtrlDict.water_valve_signal): bool to int , can not plot bool in Grafana

--- sprinkler/dict_def.py
+++ sprinkler/dict_def.py
@@ -4,7 +4,7 @@ from typing import TypedDict
 class SprinklerCtrlDict(TypedDict):
     controller_type: str
     tag: str
-    water_valve_signal: bool
+    water_valve_signal: int
     soil_moisture_min_level: float
     soil_moisture_max_level: float
 
