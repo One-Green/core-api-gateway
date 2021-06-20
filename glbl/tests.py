f361586ef4bc0c57250943775cfcc56110349bf9
shanisma <shanmugathas.vigneswaran@outlook.fr>
2021-06-20 03:02:43 +0200

    feat(glbl.tests.GlobalConfigTest):  init timezone setting

--- glbl/tests.py
+++ glbl/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from glbl.models import Config
+from django.urls import reverse
 
-# Create your tests here.
+
+class GlobalConfigTest(APITestCase):
+
+    def test_create_timezone(self):
+        url = reverse("global-config")
+        data = {'timezone': 'Europe/Paris'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Config.objects.count(), 1)
+        self.assertEqual(Config.objects.get().timezone, 'Europe/Paris')
