1a080f436363e71e999af0ba69cab0c4a97c8b50
Vigneswaran Shanmugathas <vigneswaran.shanmugathas@flowbird.group>
2022-08-13 14:51:10 +0200

    fix(sprinkler/simulate_sensors.py): remove tag registry

--- sprinkler/simulate_sensors.py
+++ sprinkler/simulate_sensors.py
@@ -14,28 +14,20 @@ from project.settings import MQTT_PASSWORD
 from project.settings import MQTT_SPRINKLER_SENSOR_TOPIC
 import time
 from random import randint
-from rest_framework.test import APIRequestFactory
 from influx_line_protocol import Metric
-from sprinkler.views import RegistryView
 
 mqtt_client = mqtt.Client()
 mqtt_client.username_pw_set(username=MQTT_USERNAME, password=MQTT_PASSWORD)
 mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
 
 sprinklers = ["tomato", "eggplant", "watermelon", "potato"]
-factory = APIRequestFactory()
-view = RegistryView.as_view()
-
-for _ in sprinklers:
-    view(factory.delete("sprinkler/registry", {"tag": _}))
-    view(factory.post("sprinkler/registry", {"tag": _}))
 
 while True:
     for _ in sprinklers:
         metric = Metric("sprinkler")
         metric.add_tag("tag", _)
         metric.add_value("soil_moisture_raw_adc", randint(20, 500))
-        metric.add_value("soil_moisture", randint(20, 500))
+        metric.add_value("soil_moisture", randint(20, 100))
         print(metric)
         mqtt_client.publish(MQTT_SPRINKLER_SENSOR_TOPIC, str(metric))
         time.sleep(1)
