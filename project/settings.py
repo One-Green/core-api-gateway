21c4e535dfad5488b346e393cef68b876ee25956
Vigneswaran Shanmugathas <vigneswaran.shanmugathas@flowbird.group>
2022-08-14 10:53:58 +0200

    feat(glbl.pagination.PageNumberPagination, project.settings): add key "total_pages"

--- project/settings.py
+++ project/settings.py
@@ -153,8 +153,8 @@ REST_FRAMEWORK = {
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
     ),
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "DEFAULT_PAGINATION_CLASS": "glbl.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
 }
 
 # CELERY + REDIS configuration
